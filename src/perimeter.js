6e23fbe306f9982a940b83c2225c58a8d8dd166b
Ellis Halliday <1478pop@gmail.com>
2019-10-21 13:13:07 -0400

    Exercise 3 Done

---
+++ src/perimeter.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-sync");
+
+let w = readlineSync.question("Width: ");
+let l = readlineSync.question("Length: ");
+const mathW = w * 2;
+const mathL = l * 2;
+const mathMid = mathW + mathL;
+let mathFinal = (mathMid* 2.54);
+mathFinal = Math.round(mathFinal*100)/100;
+let f = mathFinal.toLocaleString();
+
+console.log("A(n) " + w + "-by-"+l+"-inch sheet of paper has a perimeter of " + f +" centimeter(s).");
